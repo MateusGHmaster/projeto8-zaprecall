cfe39fa4c1b8da39fa6acdcfe1613d55a7f07f60
Mateus <mateusoliveira469@gmail.com>
2022-03-21 15:11:26 -0400

    game-finishes-v1.0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,35 @@
 import React from "react";
+import finishedHappy from "../media/finished.png";
+import finishedSad from "../media/finished.jpg";
 
 export default function Footer (props) {
 
     const toBePlayed = props.cardData.filter(item => item.status !== '').length;
-    console.log(toBePlayed);
+    const isAnyWrong = props.cardData.filter(wrong => wrong.status === 'incorrect').length;
 
-    return (
+    return toBePlayed < 4 ? (
 
         <div className="footer-counter">{toBePlayed}/4 CONCLUÍDOS</div>
-
+    
+    ) : (
+        isAnyWrong > 0 ? (
+            <>
+                <div className="final-result">
+                    <img src={finishedSad} alt="bad-ending" width={23} height={23}/>
+                    <span className="sad-span">Putz...</span>
+                </div>
+                <p className="sad-text">Ainda faltam alguns...<br/>Mas, não desanime!</p>
+                <div className="footer-counter">{toBePlayed}/4 CONCLUÍDOS</div>
+            </>
+        ) : (
+            <>
+                <div className="final-result">
+                    <img src={finishedHappy} alt="happy-ending" width={23} height={23}/>
+                    <span className="happy-span">Parabéns!</span>
+                </div>
+                <p className="happy-text">Você não esqueceu de<br/>nenhum flashcard!</p>
+                <div className="footer-counter">{toBePlayed}/4 CONCLUÍDOS</div>
+            </>
+        )
     );
-
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -2,14 +2,13 @@ import React from "react";
 
 export default function Question (props) {
     
-    let changeClassName = 'initial-class';
     const [collapsed, setCollapsed] = React.useState(true);
     const [flipped, setFlipped] = React.useState(true);
 
     return collapsed ? (
 
         <div className="closed-card" onClick={() => setCollapsed(false)}>
-            <div className="to-be-opened">{props.card.cardName}</div>
+            <div className={`to-be-opened ${props.class}`}>{props.card.cardName}</div>
             <div className="closed-icon">{props.card.cardIcon}</div>
             <ion-icon name="chevron-forward-circle-outline"></ion-icon>
         </div>
@@ -19,30 +18,26 @@ export default function Question (props) {
         flipped ? (
 
             <div className="opened-card">
-                <div className={changeClassName} >{props.card.cardQuestion}</div>
+                <div className={props.class} >{props.card.cardQuestion}</div>
                 <div className="flip-card" onClick={() => setFlipped(false)}><ion-icon name="reload-outline"></ion-icon></div>
             </div>
 
         ) : (
 
             <div className="flipped-card">
-                <div className="to-be-closed" onClick={() => setCollapsed(true)}>{props.card.cardAnswer}</div>
+                <div className='to-be-closed' onClick={() => setCollapsed(true)}>{props.card.cardAnswer}</div>
                 <div className="buttons">
                     <button type="button" className="not-zap" onClick={() => {
                         props.handleQuestions(props.index, 'incorrect');
                         setCollapsed(true);
-                        changeClassName = props.class;
-                        console.log(changeClassName);
                         }}>Não lembrei</button>
                     <button type="button" className="almost-zap" onClick={() => {
                         props.handleQuestions(props.index, 'partially-correct');
                         setCollapsed(true);
-                        changeClassName = props.class;
                         }}>Quase lembrei</button>
                     <button type="button" className="zap" onClick={() => {
                         props.handleQuestions(props.index, 'correct');
                         setCollapsed(true);
-                        changeClassName = props.class;
                         }}>Zap!</button>
                 </div>
             </div>
